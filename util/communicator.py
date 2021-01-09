999625f37f51ad2191e4729010c39ac26ebb493a
marianadangelo <dangelo.ms@gmail.com>
2021-01-08 19:06:54 -0500

    Cleaned up communication code - some methods to send arbitrary encrypted data and also send and wait for an ack from the server

--- util/communicator.py
+++ util/communicator.py
@@ -26,55 +26,50 @@ class Communicator:
 		Creates a connection to the server at (HOST, PORT) using IPv4 and TCP. 
 
 		Returns:
-			Socket: Plaintext of the decrypted data.
+			Socket: Communication channel between client and server. 
 		"""
 		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		server_socket.connect((self.HOST, self.PORT))
 
 		return server_socket
 
-	def send_to_server(self, message):
+	def encrypt_and_send(self, message):
 		"""Sends a message to the server.
 		
-		Encrypts the provided message and sends it to the connected server.
+		Encrypts the provided message and sends it over the socket.
 
 		Args:
 			message (bytes): Data to be sent.
 		"""
-
 		(ciphertext, tag, nonce) = self.cipher.encrypt(message)
 
+		# The only length that needs to be computed is that of the ciphertext
+		# as all other items (tag, nonce) are of fixed length. 
 		ciphertext_length = len(ciphertext).to_bytes(32, byteorder='big')
 
-		self.socket.sendall(ciphertext_length)
+		self.send_data(ciphertext_length)
+		self.send_data(ciphertext)
+		self.send_data(tag)
+		self.send_data(nonce)
 
-		ack = self.socket.recv(28)
-		if not ack: # TODO: make this raise an exception?
-			print("Failed to receive acknowledgement")
-			return
+	def send_data(self, data):
+		"""Sends data using the socket.
+		
+		Sends data to the recipient and waits to for an acknowledgement of receipt.
+		
+		Args:
+			data (bytes): Data to be sent.
 
-		self.socket.sendall(ciphertext)
+		Raises:
+      		ConnectionError: If the acknowledgement fails to be received.
+		"""
 
-		ack = self.socket.recv(3)
-		if not ack: # TODO: make this raise an exception?
-			print("Failed to receive acknowledgement")
-			return
-
-		self.socket.sendall(tag)
-
-		ack = self.socket.recv(3)
-		if not ack: # TODO: make this raise an exception?
-			print("Failed to receive acknowledgement")
-			return
-
-		self.socket.sendall(nonce)
+		self.socket.sendall(data)
 
 		ack = self.socket.recv(3)
-		if not ack: # TODO: make this raise an exception?
-			print("Failed to receive acknowledgement")
-			return
-
+		if not ack: 
+			raise ConnectionError("Failed to receive acknowledgement")
 
 if __name__ == '__main__':
 	communicator = Communicator()
-	communicator.send_to_server("test".encode('utf8'))
+	communicator.encrypt_and_send("test".encode('utf8'))
