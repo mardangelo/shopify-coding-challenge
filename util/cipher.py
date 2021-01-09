3a9c6b5ff7ac75e3b9d3307a713cfe64ef1c819b
marianadangelo <dangelo.ms@gmail.com>
2021-01-09 15:26:36 -0500

    Communicator can receive encrypted data correctly - Can use communicator with the server created socket - Cleaned up code by replacing some constants for cryptographic sizes with variables for easy adjustment

--- util/cipher.py
+++ util/cipher.py
@@ -22,6 +22,11 @@ class Cipher:
 	slower than other modes. 
 
 	Attributes:
+		KEY_SIZE (int): Size in bytes of the AES key.
+		LENGTH_SIZE (int): Size in bytes of the integer length of the ciphertext.
+		TAG_SIZE (int): Size in bytes of the AES-EAX tag (used for authentication). 
+		NONCE_SIZE (int): Size in bytes of the nonce (prevents replay attacks).
+
 		key (str): Secret key used for encrypting messages. 
 		key_path (str): The path to the secret.key file. Because the communicator is in a 
 						seperate module, we ensure that the same key is used for server and
@@ -29,7 +34,17 @@ class Cipher:
 						always saving it in the root of that directory. 
 	"""
 
+	KEY_SIZE = 16
+	LENGTH_SIZE = 32
+	TAG_SIZE = 16
+	NONCE_SIZE = 16
+
 	def __init__(self):
+		"""Initializes a Cipher object.
+		
+		Determines what the path to the project directory is and either creates the secret 
+		key there or retrieves it if it already exists.
+		"""
 		self.key = None
 		self.key_path = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
 		self.retrieve_secret_key()
@@ -40,8 +55,11 @@ class Cipher:
 		Generates a 128 bit AES key and saves it to disk as secret.key
 		As this is a symmetrical encryption scheme this key must be 
 		shared between the client and the server (securely). 
+
+		Returns:
+			bytes: The secret key that was created. 
 		"""
-		key = os.urandom(16) 
+		key = os.urandom(self.KEY_SIZE) 
 
 		with open(self.key_path + '/secret.key', 'wb') as f:
 			f.write(key)
@@ -56,10 +74,9 @@ class Cipher:
 		Checks disk for secret key and loads into memory. If the secret key 
 		does not exist, it is generated. 
 		""" 
-
 		if os.path.isfile(self.key_path + '/secret.key'):
 			with open(self.key_path + '/secret.key', 'rb') as f:
-				self.key = f.read(16)
+				self.key = f.read(self.KEY_SIZE)
 		else:
 			self.key = self.create_secret_key()
 
@@ -67,7 +84,7 @@ class Cipher:
 		"""Encrypts given message using secret key.
 		
 		Encrypts the message using AES128-EAX authenticated encryption.
-		Simulatenously provides authentication and privacy of message. 
+		Simultaneously provides authentication and privacy of message. 
 		Support messages of arbitrary length (i.e., no padding required).
 		
 		Args:
@@ -78,7 +95,6 @@ class Cipher:
 			bytes: Tag used to authenticate the ciphertext.
 			bytes: Number used only once, typically used to prevent replay attacks.
 		"""
-
 		cipher = AES.new(self.key, AES.MODE_EAX)
 		(ciphertext, tag) = cipher.encrypt_and_digest(message)
 
@@ -97,7 +113,6 @@ class Cipher:
 		Returns:
 			bytes: Plaintext of the decrypted data.
 		"""
-
 		cipher = AES.new(self.key, AES.MODE_EAX, nonce)
 		message = cipher.decrypt_and_verify(ciphertext, tag)
 
--- util/communicator.py
+++ util/communicator.py
@@ -1,23 +1,44 @@
 import socket 
-import sys
-from cipher import Cipher
+from .cipher import Cipher
 
 class Communicator:
 	"""Utility class for managing communication between client and server.
 	
-	[description]
+	Provides functions for communicating over the network given a socket which is
+	connected between client and server. The most important methods are encrypt_and_send 
+	and receive_and_decrypt. Note that these methods accept and produce bytes and that
+	any strings should be encoded and decoded (e.g., as utf8) as necessary. 
 
 	Attributes:
 		HOST (str): Host name or IP address of the server. 
 		PORT (int): Port open to receive connections on the server.
-		socket (Socket): Socket representing the connection between client and server.
+		ACK_SIZE (int): The size of an acknowledgement that a message has been received
+						over the communication channel. 
+						
+		socket (Socket): Represents the connection between client and server.
+		cipher (Cipher): Manages cryptographic operations.  
 	"""
 
 	HOST = '127.0.0.1'
 	PORT = 65432
 
-	def __init__(self):
-		self.socket = self.connect_to_server()
+	ACK_SIZE = 3
+
+	def __init__(self, socket=None):
+		"""Initializes a Communicator object.
+		
+		If a socket object is not provided, then one is created. This should be used 
+		by a client to create a connection to the server. A server should create the 
+		socket (after accepting a connection from the client) and then pass it into 
+		this communicator object for use. 
+		
+		Args:
+			socket (Socket): A connection between a client and a server.  (default: {None})
+		"""
+		if socket == None:
+			self.socket = self.connect_to_server()
+		else:
+			self.socket = socket
 		self.cipher = Cipher()
 
 	def connect_to_server(self):
@@ -33,6 +54,7 @@ class Communicator:
 
 		return server_socket
 
+	#TODO: encrypt and send for images? or somehow detect that the message is str/image?
 	def encrypt_and_send(self, message):
 		"""Sends a message to the server.
 		
@@ -45,7 +67,7 @@ class Communicator:
 
 		# The only length that needs to be computed is that of the ciphertext
 		# as all other items (tag, nonce) are of fixed length. 
-		ciphertext_length = len(ciphertext).to_bytes(32, byteorder='big')
+		ciphertext_length = len(ciphertext).to_bytes(Cipher.LENGTH_SIZE, byteorder='big')
 
 		self.send_data(ciphertext_length)
 		self.send_data(ciphertext)
@@ -63,13 +85,62 @@ class Communicator:
 		Raises:
       		ConnectionError: If the acknowledgement fails to be received.
 		"""
-
 		self.socket.sendall(data)
 
-		ack = self.socket.recv(3)
+		ack = self.socket.recv(self.ACK_SIZE)
 		if not ack: 
 			raise ConnectionError("Failed to receive acknowledgement")
 
-if __name__ == '__main__':
-	communicator = Communicator()
-	communicator.encrypt_and_send("test".encode('utf8'))
+	def receive_and_decrypt(self):
+		"""Receives a message. 
+		
+		Receives a message over the socket and decrypts it. 
+		
+		Returns:
+			bytes: Plaintext of the decrypted data.
+		"""
+		# Of the three pieces of information sent, only the ciphertext is variable
+		# length so we determine its length before attempting to receive it 
+		length = self.receive_data(Cipher.LENGTH_SIZE)
+		ciphertext_length = int.from_bytes(length, byteorder='big')
+
+		ciphertext = self.receive_data(ciphertext_length)
+		tag = self.receive_data(Cipher.TAG_SIZE)
+		nonce = self.receive_data(Cipher.NONCE_SIZE)
+
+		data = self.cipher.decrypt(ciphertext, tag, nonce)
+
+		return data
+
+	def acknowledge_receipt(self):
+		"""Sends an acknowledgement.
+		
+		Sends an ack signal to the other party. Note that this signal is intentionally
+		not encrypted because it does not give away any extraneous information and 
+		the overhead of encrypting these messages would not be worth it. 
+		"""
+		self.socket.sendall('ack'.encode('utf8'))
+
+	def receive_data(self, length):
+		"""Receives data.
+		
+		Receives data over the socket and sends an acknowledgement that the data has 
+		been received. 
+		
+		Args:
+			length: Length of the item being received. 
+		
+		Returns:
+			bytes: Data received from the other party.
+		
+		Raises:
+			ConnectionError: If data has not been received.
+		"""
+		data = self.socket.recv(length)
+
+		if not data:
+			raise ConnectionError("Failed to receive data")
+
+		self.acknowledge_receipt()
+
+		return data
