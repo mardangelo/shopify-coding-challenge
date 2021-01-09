4e56664e027c63615132a95d2b6a9d27a656e354
marianadangelo <dangelo.ms@gmail.com>
2021-01-09 15:42:47 -0500

    Forgot to add the enum classes

---
+++ util/command.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class Command(Enum):
+	LOGIN = 'LOGIN'
+	ADD_IMAGE = 'ADD_IMAGE'
---
+++ util/status.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class Status(Enum):
+	SUCCESS = 'SUCCESS'
+	FAILURE = 'FAILURE'
